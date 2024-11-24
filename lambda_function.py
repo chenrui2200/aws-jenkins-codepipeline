a2131d27d6f7ee2f616684457a4aa71e18e198b1
ChenRui <370299412@qq.com>
2024-11-24 14:55:42 +0800

    test

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
 
-    # 我想测试下提交更新aws
+    # 我想测试下提交更新aws， 现在时间是2023年11月11日
     print('context', context)
     return {
         'statusCode': 200,
