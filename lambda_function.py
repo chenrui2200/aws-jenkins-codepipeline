8c71cdf52ec719657c40d9e2bd3f7acac35a956e
ChenRui <370299412@qq.com>
2024-11-24 14:52:14 +0800

    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,9 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+
+    # 我想测试下提交更新aws
+    print('context', context)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
